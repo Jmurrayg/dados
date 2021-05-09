ef6cda84e1123800db463a975e2164d6bb0c7645
Jmurrayg <jmurrayg@hotmail.com>
2021-05-08 18:26:54 -0700

    se agrego iconos de cada dado y cambio for por foreach

--- src/components/dados.js
+++ src/components/dados.js
@@ -1,12 +1,20 @@
 import React, { useState,useContext, useEffect } from 'react';
-import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
-import { faDiceD6 } from '@fortawesome/free-solid-svg-icons';
+/* import { FontAwesomeIcon  } from '@fortawesome/react-fontawesome';
+import { faDiceD6 } from '@fortawesome/free-solid-svg-icons'; */
 import {DadosContext} from '../contex/DadosContex';
+import { Icon, InlineIcon } from '@iconify/react';
+import diceD4 from '@iconify-icons/mdi/dice-d4';
+import diceD6 from '@iconify-icons/mdi/dice-d6';
+import diceD8 from '@iconify-icons/mdi/dice-d8';
+import diceD10 from '@iconify-icons/mdi/dice-d10';
+import diceD12 from '@iconify-icons/mdi/dice-d12';
+import diceD20 from '@iconify-icons/mdi/dice-d20';
 
 
 function Dados({id}) {
   
    const[ state, setState] = useContext(DadosContext);
+   const[icono,setIcono] = useState(<Icon icon={diceD4} width="32px"/>)
 
     const [caras, setCaras] = useState(4)
 /*     const [resultado, setResultado] = useState(); 
@@ -42,11 +50,12 @@ function Dados({id}) {
       setTimeout(primerCara,10)
     }, [])
 
+    
+
     const cambiarCaras = (e) =>{
       const numeroCaras = e.target.value;
       //console.log(id,numeroCaras);
       setCaras(numeroCaras);
-      
       const check = [...state];
       //console.log(check[id]);
       if(check[id] === undefined){
@@ -59,25 +68,49 @@ function Dados({id}) {
         setState(check);
       }
       
+      if (numeroCaras === "4") {
+        setIcono(<Icon icon={diceD4} width="32px"/>);
+      }if (numeroCaras === "6") {
+        setIcono(<Icon icon={diceD6} width="32px"/>);
+      }if (numeroCaras === "8") {
+        setIcono(<Icon icon={diceD8} width="32px"/>);
+      }if (numeroCaras === "10") {
+        setIcono(<Icon icon={diceD10} width="32px"/>);
+      }if (numeroCaras === "12") {
+        setIcono(<Icon icon={diceD12} width="32px"/>);
+      }if (numeroCaras === "20") {
+        setIcono(<Icon icon={diceD20} width="32px"/>);
+      }
+
       //setState({...state,numeroCaras})
       //console.log("vengo del context",state);
     }
 
+
+
    
-    const dadoi = <FontAwesomeIcon icon={faDiceD6}/>
+    //const dadoi = <FontAwesomeIcon icon={faDiceD6}/>
     return (
       <div className="App">
-        <p>{id}</p>
-        {dadoi}
-        {result}
-          <select value={caras} onChange={(e)=> cambiarCaras(e)}>
-            <option value={4}>cuatro caras</option>
-            <option value={6}>seis caras</option>
-            <option value={8}>ocho caras</option>
-            <option value={9}>nueve caras</option>
-            <option value={12}>doce caras</option>
-            <option value={20}>veinte caras</option>
-          </select>
+        <div className="text-center">
+        <h3 className="mb-3 text-2xl font-semibold text-black dark:text-gray-200">Resultado del dado: {result}</h3>
+        </div>
+        <div class="flex relative w-64 my-2 mx-16">
+         {icono}
+            <select value={caras} onChange={(e)=> cambiarCaras(e)}
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
+              <option value={4}>Cuatro caras</option>
+              <option value={6}>Seis caras</option>
+              <option value={8}>Ocho caras</option>
+              <option value={10}>Diez caras</option>
+              <option value={12}>Doce caras</option>
+              <option value={20}>Veinte caras</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
+          </div>
       </div>
     );
   }
--- src/view/home.js
+++ src/view/home.js
@@ -3,6 +3,7 @@ import React, {useContext, useState} from 'react';
 import {DadosContext} from '../contex/DadosContex';
 
 
+
 function Home() {
 
   const [state, setState] = useContext(DadosContext);
@@ -15,15 +16,15 @@ function Home() {
   ]);
      const tirarDados = () =>{
        const result = [...state];
-      for (let index = 0; index < state.length; index++) {
-        const valor = Math.floor((Math.random()* result[index].numeroDeCaras) + 1)
-        if(valor !== result[index].resultado ){
-          result[index].resultado = valor
+
+      result.forEach(result => {
+        const valor = Math.floor((Math.random()* result.numeroDeCaras) + 1)
+        if(valor !== result.resultado ){
+          result.resultado = valor
         }else{
-          result[index].resultado = Math.floor((Math.random()* result[index].numeroDeCaras) + 1)
+          result.resultado = Math.floor((Math.random()* result.numeroDeCaras) + 1)
         }
-      }
-      console.log(result);
+      });
       setState(result);
     }
 
@@ -50,26 +51,49 @@ function Home() {
 
 
   return (
-    <div className="App">
-      <p>tu total: {totalDados}</p>
-      <button onClick={agregarDado}>incrementar dado</button>
-      <button onClick={quitarDado}>quitar un dado</button>
-        <div>
-          <p>Pick your dice:</p>
-        {cantidadDados.map((numeroDeCaras,index,resultado)=>{
-          return <Dados
-          key={index}
-          id={index}
-          numeroDeCaras={numeroDeCaras}
-          resultado={resultado}
-          />
-        })}
+    <>
+
+        <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
+            <div  className="container mx-auto">
+                <div style={{backgroundColor: "#bdc3c7"}} className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+                    <div className="text-center">
+                    <h1 className="my-3 text-3xl font-semibold text-black dark:text-gray-200">Escoge tus dados</h1>
+                  {
+                    isNaN(totalDados)
+                    ?<h2 className="my-3 text-2xl font-semibold text-black dark:text-gray-200">Lanza los dados para obtener tu resultado</h2>
+                    :<h2 className="my-3 text-2xl font-semibold text-black dark:text-gray-200">Tu total de esta tirada: {totalDados}</h2>
+                  }
+                    </div>
+                  <div className="flex justify-center ">
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center mr-4" onClick={agregarDado}>+1 Dado</button>
+                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center" onClick={quitarDado}>-1 Dado</button>
+                  </div>
+                    <div>
+                    {cantidadDados.map((numeroDeCaras,index,resultado)=>{
+                      return <Dados
+                      key={index}
+                      id={index}
+                      numeroDeCaras={numeroDeCaras}
+                      resultado={resultado}
+                      />
+                    })}
+                    </div>
+                    <div className="felx mx-36 w-full justify-center">
+                      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={tirarDados}>
+                      Lanzar Dados
+                      </button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <button onClick={tirarDados}>
-        click
-        </button>
-    </div>
+
+
+
+
+    </>
   );
 }
 
 export default Home;
+
+
